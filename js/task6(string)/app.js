bf402c6f9551c8f782f840ce6e22100c2f8cefa9
Cavidan Rəcəbli <recebli.cavidan@gmail.com>
2023-05-05 09:33:43 +0400

    add

---
+++ js/task6(string)/app.js
@@ -0,0 +1,56 @@
+// let word1  = "Recebli Cavidan Ayaz"
+// let word2 = "azerbaijaaaan"
+// let  searchWord = prompt("soz axtarilan soz ")
+// const isHidden = (params , searchWord ) =>    params.includes(searchWord)
+// console.log(isHidden(word1, searchWord))
+// console.log(isHidden(word2, searchWord))
+// const fixClassName = (params , searchWord ) => params.replaceAll(" ",searchWord)
+// console.log(fixClassName(word1 , searchWord))
+
+// ------------- Task 1 ------------
+
+// const repeatedSymbolCounter = (par1 , par2) =>{
+//     let count = 0;
+//     for (let i = 0; i < par1.length; i++) {
+//         par1[i] === par2   ? count++  : false
+//     }
+//     console.log(count)
+// }
+
+//  repeatedSymbolCounter(word2 , searchWord)
+
+// ------------ Task 2 ------------
+// const capitalize  = str =>{
+//     const str2 = str.charAt(0).toUpperCase() + str.slice(1);
+//     return str2
+// }
+// console.log(capitalize(word2))
+
+// ----------- Task3  --------------
+// const fixClassName = (params , searchWord ) => params.replaceAll(" ",searchWord)
+// console.log(fixClassName(word1 , searchWord))
+
+// -------------Task 4 ------------
+// 
+// ------------ Task 5 -----------------
+// const  changeFirstWord  = (par1 , searchWord) =>{
+//     const par2 = par1.charAt(0).toUpperCase() + par1.slice(1);
+//     return searchWord.replace(par2)
+// }
+// console.log(changeFirstWord(word2, searchWord))
+// -------------------------------------------------------------------------    
+// const sliceJs = par =>{
+//     par1 = " "
+//    for (let i = 0; i < par.length; i++) {
+//         par1 += par.slice(" ")  
+//    }
+//    return par1
+// }
+// console.log(sliceJs(word1))
+// ------------ Task 3 -----------
+// const removeCharacterFromString = (par1 , searchWord) =>{   
+//     if(par1.charAt(0) === searchWord){
+//      return par1 = par1.substring(1);
+//     }
+// }
+//console.log(removeCharacterFromString("Alion","A"))
