1589691f5332523450dfa2706baa8529b83f1778
Cavidan Rəcəbli <recebli.cavidan@gmail.com>
2023-05-08 00:08:32 +0400

    complete tasks

--- js/task8/app.js
+++ js/task8/app.js
@@ -1,35 +1,29 @@
-//////////////////// Task 1 ------
-let num = prompt("eded dacil edin");
-num = parseInt(sayi);
-while (num < 100 || num > 999) {
-  num += 7;
-}
-
+// ----------------Task 1 --------- 
+let num = +prompt("eded dacil edin");
+while (num < 100 || num > 999) {  num += 7;     }
 console.log("netice " + num);
 
-//////////////////////// Task 2 ///////////
-let n = parseInt(prompt(" yaz"));
+// ----------------Task 2--------- 
+let n = +prompt(" yaz");
 for (let i = 1; i <= n; i++) {
   console.log("for dongusu istifadesi");
 }
 // ----------------Task 3 --------- 
 let sum = 0;
-for (let i = 11; i <= 99; i += 2) {
-  sum += i;
-}
+for (let i = 11; i <= 99; i += 2) {  sum += i;    }
 console.log("Iki Reqemli Ededlerin Cemi " + toplam);
 
 // ----------------Task 4 --------- 
 function isLucky(bilet) {
     let bilet_str = bilet.toString().padStart(6, "0");
-    let sum1 = parseInt(bilet_str[0]) + parseInt(bilet_str[1]) + parseInt(bilet_str[2]);
-    let sum2 = parseInt(bilet_str[3]) + parseInt(bilet_str[4]) + parseInt(bilet_str[5]);
+    let sum1 =   +bilet_str[0] +   +bilet_str[1] +   +bilet_str[2];
+    let sum2 =   +bilet_str[3] +   +bilet_str[4] +   +bilet_str[5];
     return sum1 === sum2;
   }
   console.log(isLucky(123321));   
 
 // ----------------Task 5 --------- 
-const inputDate = prompt("Please enter the date in the format of YYYY.MM.DD:");
+const inputDate = prompt("Date formatini daxil edin:  YYYY.MM.DD:");
 const dateArray = inputDate.split(".");
 let outputDate;
 if (dateArray.length === 3 && dateArray[0].length === 4 && dateArray[1].length === 2 && dateArray[2].length === 2) {
@@ -39,15 +33,15 @@ if (dateArray.length === 3 && dateArray[0].length === 4 && dateArray[1].length =
   const dateObject = new Date(`${month}/${day}/${year}`);
   outputDate = dateObject.toLocaleDateString("en-US", { month: "long", day: "numeric", year: "numeric" });
 } else {
-  outputDate = "Invalid value entered";
+  outputDate = "olmadi !";
 }
 console.log(outputDate);
 
 // ----------------Task 6 --------- 
 function numberToText() {
-    const number = parseInt(prompt("Please enter a number between 1 and 99:"));
+    const number = parseInt(prompt("1 le 99 arasinda eded daxil edin:"));
     if (isNaN(number) || number < 1 || number > 99) {
-      console.log("Invalid number entered");
+      console.log("loll");
       return;
     }
     const ones = ["", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen"];
@@ -66,7 +60,7 @@ function numberToText() {
   
 
 // ----------------Task 7 --------- 
-const fullName = prompt("Please enter your full name (in the format 'FIRSTNAME'):");
+const fullName = prompt("Tam Adinizi daxil edin  'FIRSTNAME':");
 const lastName = fullName.split(" ")[1];
 console.log(lastName);
 
