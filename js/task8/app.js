e089d28c815f0d65949f000b8d2448af776711e7
Cavidan Rəcəbli <recebli.cavidan@gmail.com>
2023-05-07 18:24:07 +0400

    A

--- js/task8/app.js
+++ js/task8/app.js
@@ -1,7 +1,23 @@
+//////////////////// Task 1 ------
 let num = prompt("eded dacil edin");
 num = parseInt(sayi);
 while (num < 100 || num > 999) {
   num += 7;
 }
 
-console.log("netrice " + num);
+console.log("netice " + num);
+
+//////////////////////// Task 2 ///////////
+let n = parseInt(prompt(" yaz"));
+for (let i = 1; i <= n; i++) {
+  console.log("for dongusu istifadesi");
+}
+
+// 
+let sum = 0;
+
+for (let i = 11; i <= 99; i += 2) {
+  sum += i;
+}
+
+console.log("İki basamaklı tek sayıların toplamı: " + toplam);
